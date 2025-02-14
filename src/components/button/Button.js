9c34cde8488444b30b40eb9cbd6d5e7dcd376368
karthikjohnn <karthickyamkk2001@gmail.com>
2025-02-14 09:43:04 +0530

    login and sign in patch

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 function Button({ type = "button", name = "button name" ,onclick,disable="false"}) {
   return (
     <button
-      className="w-full bg-gradient-to-bl from-accent-500  to-accent-400 transition-all duration-1000 ease-in-out hover:bg-gradient-to-br focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center text-white"
+      className="min-h-11 w-full bg-gradient-to-bl from-accent-500  to-accent-400 transition-all duration-1000 ease-in-out hover:bg-gradient-to-br focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center text-white"
       type={type}
       onClick={onclick ? () => onclick() : undefined}
       disabled={disable}
